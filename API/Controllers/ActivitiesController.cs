23af47e817c0041338178b8c14f6298c5ff448d3
Kap Lim <freekaplim@gmail.com>
2019-10-31 03:11:14 -0400

    Added authentication in server

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -4,24 +4,25 @@ using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ActivitiesController : Controller
+    // [Route("api/[controller]")]
+    // [ApiController]
+    public class ActivitiesController : BaseController  //Controller
     {
-        private readonly IMediator _mediator;
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+        // private readonly IMediator _mediator;
+        // public ActivitiesController(IMediator mediator)
+        // {
+        //     _mediator = mediator;
+        // }
         
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> List()
         {
-            return await _mediator.Send(new ListActivities.Query());
+            return await Mediator.Send(new ListActivities.Query());
         }
         // public async Task<ActionResult<List<Activity>>> List(
         //     CancellationToken cancellationToken)
@@ -31,16 +32,17 @@ namespace API.Controllers
         // }
 
         [HttpGet("{id}")]
+        //[Authorize]
         public async Task<Activity> Details(Guid id)
         {
-            return await _mediator.Send(new Details.Query{Id = id});
+            return await Mediator.Send(new Details.Query{Id = id});
         }
 
         [HttpPost]
         public async Task<ActionResult<Unit>> Create(
             [FromBody]Create.Command command)
         {
-            return await _mediator.Send(command);
+            return await Mediator.Send(command);
         }
 
         [HttpPut("{id}")]
@@ -48,13 +50,13 @@ namespace API.Controllers
             [FromBody]Edit.Command command)
         {
             command.Id = id;
-            return await _mediator.Send(command);
+            return await Mediator.Send(command);
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<Unit>> Delete(Guid id)
         {
-            return await _mediator.Send(new Delete.Command{Id = id});
+            return await Mediator.Send(new Delete.Command{Id = id});
         }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -16,6 +16,15 @@ using MediatR;
 using Application.Activities;
 using FluentValidation.AspNetCore;
 using API.Middleware;
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Application.Interfaces;
+using Infrastructure.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace API
 {
@@ -41,10 +50,39 @@ namespace API
                 });
             });
             services.AddMediatR(typeof(ListActivities.Handler).Assembly);
-            services.AddMvc()
+            services.AddMvc(opt =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser().Build();
+                opt.Filters.Add(new AuthorizeFilter(policy));
+            })
                 .AddFluentValidation(config =>
                     config.RegisterValidatorsFromAssemblyContaining<Create>())
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var builder = services.AddIdentityCore<AppUser>();
+            var identityBuilder = new IdentityBuilder(
+                builder.UserType, builder.Services);
+            identityBuilder.AddEntityFrameworkStores<DataContext>();
+            identityBuilder.AddSignInManager<SignInManager<AppUser>>();
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt =>
+                {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = key,
+                        ValidateAudience = false,
+                        ValidateIssuer = false
+                    };
+                });
+
+            services.AddScoped<IJwtGenerator, JwtGenerator>();
+            services.AddScoped<IUserAccessor, UserAccessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,6 +100,7 @@ namespace API
             }
 
             //app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseCors("CorsPolicy");
             app.UseMvc();
         }
