8a203caf94143188351ca9ec85e815ebf5e3bfd5
Kap Lim <freekaplim@gmail.com>
2019-11-10 22:07:00 -0500

    Added attendance feature

--- react-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ react-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,23 +1,46 @@
 import React from 'react'
-import { Item, Button, Segment, Icon } from 'semantic-ui-react'
+import { Item, Button, Segment, Icon, Label } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import { IActivity } from '../../../app/models/activity';
 import { format } from 'date-fns';
+import ActivityListItemAttendees from './ActivityListItemAttendees';
 
 const ActivityListItem: React.FC<{activity: IActivity}> = ({activity}) => {
-
+    const host = activity.attendees.filter(x => x.isHost)[0];
     return (
         <Segment.Group>
             <Segment>
                 <Item.Group>
                     <Item>
                         <Item.Image size='tiny' circular
-                            src='/assets/user.png' />
+                            src={host.image || '/assets/user.png'}
+                            style={{ marginBottom: 3 }}
+                        />
                         <Item.Content>
-                            <Item.Header as='a'>{activity.title}</Item.Header>
+                            <Item.Header as={Link}
+                                to={`/activities/${activity.id}`}>
+                                {activity.title}
+                            </Item.Header>
                             <Item.Description>
-                                Hosted by Bob
+                                Hosted by&nbsp;
+                                <Link to={`/profile/${host.username}`}>
+                                    {host.displayName}
+                                </Link>
                             </Item.Description>
+                            {activity.isHost && (
+                                <Item.Description>
+                                    <Label basic color='orange'
+                                        content=
+                                        'You are hosting this activity' />
+                                </Item.Description>
+                            )}
+                            {activity.isGoing && !activity.isHost && (
+                                <Item.Description>
+                                <Label basic color='green'
+                                    content=
+                                    'You are going to this activity' />
+                                </Item.Description>
+                            )}
                         </Item.Content>
                     </Item>
                 </Item.Group>
@@ -26,7 +49,9 @@ const ActivityListItem: React.FC<{activity: IActivity}> = ({activity}) => {
                 <Icon name='clock' /> {format(activity.date, 'h:mm a')}
                 <Icon name='marker' /> {activity.venue}, {activity.city}
             </Segment>
-            <Segment secondary>Attendees will go here</Segment>
+            <Segment secondary>
+                <ActivityListItemAttendees attendees={activity.attendees} />
+            </Segment>
             <Segment clearing>
                 <span>{activity.description}</span>
                 <Button as={Link} to={`/activities/${activity.id}`}
